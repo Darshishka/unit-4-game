2630de88b18bd6fe8b5202d44118e9e8af221b2b
Darcy Atlas <foreverdiffrent@live.com>
2019-08-12 18:26:41 -0700

    laid out in comments

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// crystal imade buttons on the page 
+    //(each has a random value between 1 and 12)
+//random number (between 19 and 120) shown to player 
+    //at start
+//player goal is to score (crystal press) to equal
+    //number at start
+//player lose if goes over
+
+//updates win or lossses
+//resets random number and player score to zero
