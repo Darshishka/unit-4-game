5863a4762067f3196c765b0629a6c63b345a29c6
Darcy Atlas <foreverdiffrent@live.com>
2019-08-14 20:43:27 -0700

    all done!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,81 @@
-// crystal imade buttons on the page 
-    //(each has a random value between 1 and 12)
-//random number (between 19 and 120) shown to player 
-    //at start
-//player goal is to score (crystal press) to equal
-    //number at start
-//player lose if goes over
-
-//updates win or lossses
-//resets random number and player score to zero
-
 var blueGem = Math.floor(Math.random() * 12) + 1;
 var greenGem = Math.floor(Math.random() * 12) + 1;
+while (greenGem == blueGem) {
+    greenGem = Math.floor(Math.random() * 12) + 1;
+}
 var redGem = Math.floor(Math.random() * 12) + 1;
+while (redGem == greenGem || redGem == blueGem){
+    redGem = Math.floor(Math.random() * 12) + 1;
+}
 var yellowGem = Math.floor(Math.random() * 12) + 1;
+while (yellowGem == redGem || yellowGem == greenGem || yellowGem == blueGem) {
+    yellowGem = Math.floor(Math.random() * 12) + 1;
+}
 
+var goalNumber = Math.floor(Math.random() * 101) + 19;
 
+var totalNumber = 0;
+var winNum = 0;
+var loseNum = 0;
 
-$(document).ready(function() {
-    var goalNumber = Math.floor(Math.random() * 120) + 19;
-    var totalNumber = 0;
+function testWin() {
+    if (totalNumber == goalNumber) {
+        winNum++;
+        document.getElementById("winNum").innerHTML = "Wins: " + winNum;
+        reset();
+    }
+    else if (totalNumber > goalNumber) {
+        loseNum++;
+        document.getElementById("loseNum").innerHTML = "Losses: " + loseNum;
+        reset();
+    }
+}
+
+function reset(){
+    blueGem = Math.floor(Math.random() * 12) + 1;
+    greenGem = Math.floor(Math.random() * 12) + 1;
+    while (greenGem == blueGem) {
+        greenGem = Math.floor(Math.random() * 12) + 1;
+    }
+    redGem = Math.floor(Math.random() * 12) + 1;
+    while (redGem == greenGem || redGem == blueGem){
+        redGem = Math.floor(Math.random() * 12) + 1;
+    }
+    yellowGem = Math.floor(Math.random() * 12) + 1;
+    while (yellowGem == redGem || yellowGem == greenGem || yellowGem == blueGem) {
+        yellowGem = Math.floor(Math.random() * 12) + 1;
+    }
+    totalNumber = 0;
+    goalNumber = Math.floor(Math.random() * 101) + 19;
     document.getElementById("goalNum").innerHTML = goalNumber;
     document.getElementById("totalNum").innerHTML = totalNumber;
+}
 
-    
-
-    function reset(){
-        
-    }
+$(document).ready(function() {
+    document.getElementById("goalNum").innerHTML = goalNumber;
+    document.getElementById("totalNum").innerHTML = totalNumber;
 
     $("#blueGem").click(function() {
         totalNumber = totalNumber + blueGem;
         document.getElementById("totalNum").innerHTML = totalNumber;
-        return totalNumber;
+        testWin();
     });
 
     $("#greenGem").click(function() {
         totalNumber = totalNumber + greenGem;
         document.getElementById("totalNum").innerHTML = totalNumber;
+        testWin();
     });
 
     $("#redGem").click(function() {
         totalNumber = totalNumber + redGem;
         document.getElementById("totalNum").innerHTML = totalNumber;
+        testWin();
     });
 
     $("#yellowGem").click(function() {
         totalNumber = totalNumber + yellowGem;
         document.getElementById("totalNum").innerHTML = totalNumber;
+        testWin();
     });
-
-
-
 });
