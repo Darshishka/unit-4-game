cb6547e57a865f2bdb9de85b3e84ed3e040593cf
Darcy Atlas <foreverdiffrent@live.com>
2019-08-14 18:05:02 -0700

    got styles and numbers working. working on getting it to reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,3 +8,47 @@
 
 //updates win or lossses
 //resets random number and player score to zero
+
+var blueGem = Math.floor(Math.random() * 12) + 1;
+var greenGem = Math.floor(Math.random() * 12) + 1;
+var redGem = Math.floor(Math.random() * 12) + 1;
+var yellowGem = Math.floor(Math.random() * 12) + 1;
+
+
+
+$(document).ready(function() {
+    var goalNumber = Math.floor(Math.random() * 120) + 19;
+    var totalNumber = 0;
+    document.getElementById("goalNum").innerHTML = goalNumber;
+    document.getElementById("totalNum").innerHTML = totalNumber;
+
+    
+
+    function reset(){
+        
+    }
+
+    $("#blueGem").click(function() {
+        totalNumber = totalNumber + blueGem;
+        document.getElementById("totalNum").innerHTML = totalNumber;
+        return totalNumber;
+    });
+
+    $("#greenGem").click(function() {
+        totalNumber = totalNumber + greenGem;
+        document.getElementById("totalNum").innerHTML = totalNumber;
+    });
+
+    $("#redGem").click(function() {
+        totalNumber = totalNumber + redGem;
+        document.getElementById("totalNum").innerHTML = totalNumber;
+    });
+
+    $("#yellowGem").click(function() {
+        totalNumber = totalNumber + yellowGem;
+        document.getElementById("totalNum").innerHTML = totalNumber;
+    });
+
+
+
+});
